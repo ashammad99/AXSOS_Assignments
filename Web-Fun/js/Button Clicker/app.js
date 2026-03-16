f7c3b5c68f1086ae48132cbaa12b241766cd724a
Ahmed Hammad <ashammad99@gmail.com>
2026-03-16 22:56:58 +0200

    Button Click Assigment submission

---
+++ Web-Fun/js/Button Clicker/app.js
@@ -0,0 +1,10 @@
+function loginButton(element) {
+    element.innerText = "Logout";
+}
+function remove(element) {
+    element.remove();
+}
+
+function alertClick() {
+    alert("Ninja was liked");
+}
