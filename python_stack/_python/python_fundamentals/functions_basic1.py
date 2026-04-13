76c6a6776bed306674296e6f5c74ed88f9159011
Ahmed Hammad <ashammad99@gmail.com>
2026-04-13 10:42:35 +0300

    Functions Basic 1 Submission

---
+++ python_stack/_python/python_fundamentals/functions_basic1.py
@@ -0,0 +1,120 @@
+#1 #My Expected Result: 5
+def a():
+    return 5
+print(a())
+
+#2 #My Expected Result: 10
+def a():
+    return 5
+print(a()+a())
+
+#3 #My Expected Result: 5
+def a():
+    return 5
+    return 10
+print(a())
+
+#4 #My Expected Result: 5
+def a():
+    return 5
+    print(10)
+print(a())
+
+#5 #My Expected Result: 5, None, because the function is void type
+def a():
+    print(5)
+x = a()
+print(x)
+
+#6 #My Expected Result: 3,5, Error
+def a(b,c):
+    print(b+c)
+print(a(1,2) + a(2,3))
+
+#7 #My Expected Result: "25"
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5))
+
+#8 #My Expected Result: 100,10
+def a():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(a())
+
+#9 #My Expected Result: 7,14,21
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3))
+print(a(5,3))
+print(a(2,3) + a(5,3))
+
+#10 #My Expected Result: 8
+def a(b,c):
+    return b+c
+    return 10
+print(a(3,5))
+
+#11 #My Expected Result: 500, 500, 300, 500
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+print(b)
+a()
+print(b)
+
+
+#12 #My Expected Result: 500, 500, 300, 500
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+a()
+print(b)
+
+#13 #My Expected Result: 500, 500, 300, 300
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=a()
+print(b)
+
+#14 #My Expected Result: 1, 3, 2
+def a():
+    print(1)
+    b()
+    print(2)
+def b():
+    print(3)
+a()
+
+
+#15 #My Expected Result: 1, 3,5, 10
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
